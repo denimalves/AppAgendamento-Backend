6809dafda4d0b758aa74aa753d561623e2a17636
Victoria's Mobile - Denner C Alves <dencunalves@gmail.com>
2025-05-11 15:17:54 -0300

    Update index.js

--- backendNovo/index.js
+++ backendNovo/index.js
@@ -1,16 +1,15 @@
-// models/index.js
-import { Sequelize } from 'sequelize'
+import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('agenda', 'root', '12345678', {
-  host: 'localhost',
-  dialect: 'mysql',
-})
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
-try {
-  await sequelize.authenticate()
-  console.log('Conexão com MySQL estabelecida')
-} catch (err) {
-  console.error('Erro na conexão com MySQL:', err)
-}
-
-export default sequelize
+export default sequelize;
